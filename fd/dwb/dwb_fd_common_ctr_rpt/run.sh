3cc4c1eb87c8a70722e01c5e6e4eda1b596b4cff
ekko zhang <ekko.zhang@i9i8.com>
2020-12-04 12:41:50 +0800

    common ctr spark-sql1

--- fd/dwb/dwb_fd_common_ctr_rpt/run.sh
+++ fd/dwb/dwb_fd_common_ctr_rpt/run.sh
@@ -39,12 +39,12 @@ create table tmp.tmp_fd_common_ctr as
 SELECT
 /*+ REPARTITION(5) */
 event_name,
-nvl(platform_type,'NALL'),
-nvl(country,'NALL'),
-nvl(project,'NALL'),
-nvl(page_code,'NALL'),
-nvl(element_event_struct.list_type,'NALL') list_name,
-nvl(element_event_struct.element_name,'NALL') element_name,
+nvl(platform_type,'NALL') as platform_type,
+nvl(country,'NALL') as country,
+nvl(project,'NALL') as project,
+nvl(page_code,'NALL') as page_code,
+nvl(element_event_struct.list_type,'NALL') as list_name,
+nvl(element_event_struct.element_name,'NALL') as  element_name,
 session_id
 from ods_fd_snowplow.ods_fd_snowplow_element_event
 where pt='$pt'
