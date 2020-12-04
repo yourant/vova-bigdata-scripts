d62f43670dd178a3e38bae8edf3c6111ad078c55
ekko zhang <ekko.zhang@i9i8.com>
2020-12-04 18:00:10 +0800

    last7

--- fd/dwb/dwb_fd_last_7_days_goods_rpt/run.sh
+++ fd/dwb/dwb_fd_last_7_days_goods_rpt/run.sh
@@ -51,7 +51,7 @@ select
         from ods_fd_vb.ods_fd_order_info
     where   oi.email  NOT REGEXP 'tetx.com|i9i8.com|jjshouse.com|jenjenhouse.com|163.com|qq.com'
     and to_date(oi.order_time)>= date_add(to_date(from_utc_timestamp('$pt','America/Los_Angeles')),-6)
-                           and to_date(oi.order_time) < to_date(from_utc_timestamp('$pt','America/Los_Angeles'))
+                           and to_date(oi.order_time) <= to_date(from_utc_timestamp('$pt','America/Los_Angeles'))
                            and pay_status>=1
          ) oi
     inner join ods_fd_vb.ods_fd_order_goods og
