e3ad9be8057f6f591d04dca5c56bb08d5cbc0e5b
ekko zhang <ekko.zhang@i9i8.com>
2020-12-04 12:38:34 +0800

    common ctr spark-sql

--- fd/dwb/dwb_fd_common_ctr_rpt/run.sh
+++ fd/dwb/dwb_fd_common_ctr_rpt/run.sh
@@ -28,14 +28,52 @@ echo $pt_format_last
 
 #hive -hiveconf pt=$pt -f /mnt/vova-bigdata-scripts/fd/dwb.dwb_fd_common_ctr_rpt/dwb_fd_common_ctr.hql
 
-shell_path="/mnt/vova-bigdata-scripts/fd/dwb/dwb_fd_common_ctr_rpt"
+#shell_path="/mnt/vova-bigdata-scripts/fd/dwb/dwb_fd_common_ctr_rpt"
 
 #hive -hiveconf pt=$pt -f /mnt/vova-bigdata-scripts/fd/dwb/dwb_fd_common_ctr_rpt/dwb_fd_common_ctr_rpt.hql
+
+
+sql="
+drop table  if exists tmp.tmp_fd_common_ctr;
+create table tmp.tmp_fd_common_ctr as
+SELECT
+/*+ REPARTITION(5) */
+event_name,
+nvl(platform_type,'NALL'),
+nvl(country,'NALL'),
+nvl(project,'NALL'),
+nvl(page_code,'NALL'),
+nvl(element_event_struct.list_type,'NALL') list_name,
+nvl(element_event_struct.element_name,'NALL') element_name,
+session_id
+from ods_fd_snowplow.ods_fd_snowplow_element_event
+where pt='$pt'
+and event_name in ('common_impression', 'common_click')
+and session_id is not null
+group by platform_type,country,project,page_code,element_event_struct.list_type,element_event_struct.element_name,session_id,event_name;
+
+
+insert overwrite table dwb.dwb_fd_common_ctr_rpt  partition(pt='$pt')
+SELECT
+/*+ REPARTITION(1) */
+nvl(platform_type,'all'),
+nvl(country,'all'),
+nvl(project,'all'),
+nvl(page_code,'all'),
+nvl(list_name,'all') AS list_name,
+nvl(element_name,'all') AS element_name,
+count(distinct if(event_name = 'common_impression', session_id, NULL)) AS impression_uv,
+count(distinct if(event_name = 'common_click', session_id, NULL)) AS click_uv
+from tmp.tmp_fd_common_ctr
+group by platform_type,country,project,page_code,list_name,element_name with cube;
+
+
+"
 
 spark-sql \
 --conf "spark.app.name=dwb_fd_common_ctr_rpt_yjzhang"   \
 -d pt=$pt \
--f ${shell_path}/dwb_fd_common_ctr_rpt.hql
+-e "$sql"
 
 if [ $? -ne 0 ];then
   exit 1
