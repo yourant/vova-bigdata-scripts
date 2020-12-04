a6c4ba6b052d8a47e8994ed1f85ec31b7cdce869
ekko zhang <ekko.zhang@i9i8.com>
2020-12-04 17:37:57 +0800

    add file

--- fd/dwb/dwb_fd_last_7_days_goods_rpt/run.sh
+++ fd/dwb/dwb_fd_last_7_days_goods_rpt/run.sh
@@ -26,17 +26,48 @@ echo $pt
 #echo $pt_format
 #echo $pt_format_last
 
-shell_path="/mnt/vova-bigdata-scripts/fd/dwb/dwb_fd_last_7_days_goods_rpt"
+#shell_path="/mnt/vova-bigdata-scripts/fd/dwb/dwb_fd_last_7_days_goods_rpt"
 
 
 #hive -hiveconf pt=$dt -f ${shell_path}/dwb_fd_last_7_days_goods_rpt.hql
 
+
+sql="
+insert overwrite table dwb.dwb_fd_last_7_days_goods_rpt
+SELECT
+    project_name,
+    goods_id,
+    sum(goods_number) as goods_num
+FROM (
+select
+    oi.project_name,
+    oi.pay_status,
+    og.goods_id,
+    og.goods_number
+    from (select
+                order_id,
+                project_name,
+                order_time
+        from ods_fd_vb.ods_fd_order_info
+    where   oi.email  NOT REGEXP 'tetx.com|i9i8.com|jjshouse.com|jenjenhouse.com|163.com|qq.com'
+    and to_date(oi.order_time)>= date_add(to_date(from_utc_timestamp('$pt','America/Los_Angeles')),-6)
+                           and to_date(oi.order_time) < to_date(from_utc_timestamp('$pt','America/Los_Angeles'))
+                           and pay_status>=1
+         ) oi
+    inner join ods_fd_vb.ods_fd_order_goods og
+             on  oi.order_id=og.order_id
+     left join ods_fd_vb.ods_fd_goods_project gp
+     on og.goods_id=gp.goods_id and gp.is_on_sale=0
+     )tab1
+     group by project_name,goods_id
+     having sum(goods_number)>=35;
+"
+
+
 spark-sql \
 --conf "spark.app.name=dwb_fd_abtest_funnel_rate_rpt_yjzhang"   \
---conf "spark.dynamicAllocation.maxExecutors=60" \
 -d pt=$pt \
--f ${shell_path}/dwb_fd_last_7_days_goods_rpt.hql
-
+-e "$sql"
 
 #如果脚本失败，则报错
 if [ $? -ne 0 ];then
