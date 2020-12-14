d7d94317045262126fa232aac5850e3cd3568dea
jhlu <jhlu@i9i8.com>
2020-12-14 19:48:44 +0800

    修改sync shell

--- common/sync.sh
+++ common/sync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-scp -i /home/hadoop/vova-bd-ssh.pem -r /mnt/vova-bigdata-scripts/ hadoop@ip-10-108-15-103.ec2.internal:/mnt/
-scp -i /home/hadoop/vova-bd-ssh.pem -r /mnt/vova-bigdata-scripts/ hadoop@ip-10-108-7-65.ec2.internal:/mnt/
+scp -i /home/hadoop/.ssh/id_rsa -r /mnt/vova-bigdata-scripts/ hadoop@ip-10-108-3-7.ec2.internal:/mnt/
+scp -i /home/hadoop/.ssh/id_rsa -r /mnt/vova-bigdata-scripts/ hadoop@ip-10-108-9-243.ec2.internal:/mnt/
