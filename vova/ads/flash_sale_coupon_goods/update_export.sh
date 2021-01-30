46958bcfc3119c4d47ba9b35aec36a13c1e1a876
zyzheng <zyzheng@i9i8.com>
2021-01-30 15:55:20 +0800

    zyzheng

--- vova/ads/flash_sale_coupon_goods/update_export.sh
+++ vova/ads/flash_sale_coupon_goods/update_export.sh
@@ -19,6 +19,9 @@ CREATE TABLE IF NOT EXISTS themis.flash_sale_coupon_goods_new
     id               int(11)        unsigned NOT NULL AUTO_INCREMENT,
     goods_id         int(20)        NOT NULL DEFAULT '0' COMMENT '商品id',
     first_cat_id     int(10)        NOT NULL DEFAULT '0' COMMENT 'cat_id',
+    gmv              decimal(15, 2) NOT NULL DEFAULT '0' COMMENT 'gmv',
+    gcr              decimal(15, 4) NOT NULL DEFAULT '0' COMMENT 'gcr',
+    clicks           int(10)        NOT NULL DEFAULT '0' COMMENT 'clicks',
     gmv_rank         int(11)        NOT NULL DEFAULT '0' COMMENT 'gmv_rank gmv desc',
     gcr_rank         int(11)        NOT NULL DEFAULT '0' COMMENT 'gcr_rank gcr desc',
     create_time      timestamp      NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -39,7 +42,7 @@ sqoop export \
 --username bdwriter --password Dd7LvXRPDP4iIJ7FfT8e \
 --table flash_sale_coupon_goods_new \
 --m 1 \
---columns goods_id,first_cat_id,gmv_rank,gcr_rank \
+--columns goods_id,first_cat_id,gmv_rank,gcr_rank,gmv,gcr,clicks \
 --hcatalog-database ads \
 --hcatalog-table ads_flash_sale_coupon_goods \
 --hcatalog-partition-keys pt \
