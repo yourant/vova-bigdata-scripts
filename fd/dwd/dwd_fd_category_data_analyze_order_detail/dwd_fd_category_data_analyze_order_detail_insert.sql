62aaf7cef62cb1fde3b2ebd0f48984c2af4e1a83
shihu <shihu@i9i8.com>
2021-02-01 19:12:29 +0800

    '品类数据分析报表开发代码提交02'

--- fd/dwd/dwd_fd_category_data_analyze_order_detail/dwd_fd_category_data_analyze_order_detail_insert.sql
+++ fd/dwd/dwd_fd_category_data_analyze_order_detail/dwd_fd_category_data_analyze_order_detail_insert.sql
@@ -10,5 +10,5 @@ select
      project_name,
      if(country_code  in ('DE','FR','GB','PL','MX','US','IT','SE','ES','BR','CZ','NL','CL','AU','RU','AT','CO','DK','NO','CH','SK','IL','FL','SA') ,country_code  ,'others') as country
 from dwd.dwd_fd_order_goods t1
-where pay_status=2
-and order_time > unix_timestamp(concat('${pt}',' 00:00:00')) and order_time < unix_timestamp(concat(date_sub('${pt}',-1),' 00:00:00'));
+where pay_status=2 and
+date(from_unixtime(pay_time,'yyyy-MM-dd HH:mm:ss')) = '${pt}';
