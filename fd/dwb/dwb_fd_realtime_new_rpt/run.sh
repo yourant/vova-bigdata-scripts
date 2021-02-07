de4b08d85c8a6ef8e6b76c50a52716ac2d7d7657
jhlu <jhlu@i9i8.com>
2021-02-07 11:28:49 +0800

    realtime修改并加报警机制

--- fd/dwb/dwb_fd_realtime_new_rpt/run.sh
+++ fd/dwb/dwb_fd_realtime_new_rpt/run.sh
@@ -2,36 +2,19 @@
 home=`dirname "$0"`
 cd $home
 
-if [ ! -n "$1" ] ;then
-    pt=`date -d"-1 hour" +%Y-%m-%d`
-    #pt_last=`date -d "-2 days" +%Y-%m-%d`
-    #pt_format=`date -d "-1 days" +%Y%m%d`
-    #pt_format_last=`date -d "-2 days" +%Y%m%d`
+if [ "$#" -ne 3 ]; then
+  pt=$(date -d "$ts -1 hours" +"%Y-%m-%d")
+  hour=$(date -d "$ts -1 hours" +"%H")
 else
-    echo $1 | grep -Eq "[0-9]{4}-[0-9]{2}-[0-9]{2}" && date -d $1 +%Y-%m-%d > /dev/null
-    if [[ $? -ne 0 ]]; then
-        echo "接收的时间格式${1}不符合:%Y-%m-%d，请输入正确的格式!"
-        exit
-    fi
-    pt=$1
-    pt_last=`date -d "$1 -1 days" +%Y-%m-%d`
-    pt_format=`date -d "$1" +%Y%m%d`
-    pt_format_last=`date -d "$1 -1 days" +%Y%m%d`
-
+  pt=$2
+  hour=$3
 fi
 
 #hive sql中使用的变量
 echo $pt
-#echo $pt_last
-#echo $pt_format
-#echo $pt_format_last
 
 #脚本路径
 shell_path="/mnt/vova-bigdata-scripts/fd/dwb/dwb_fd_realtime_new_rpt"
-
-
-#hive -hiveconf pt=$pt -f ${shell_path}/dwb_fd_realtime_gmv_orders_rpt.hql
-
 
 spark-sql \
 --conf "spark.app.name=dwb_fd_realtime_gmv_orders_rpt_zhangchenhao"   \
@@ -45,3 +28,13 @@ if [ $? -ne 0 ];then
   exit 1
 fi
 echo "realtime_gmv_orders rpt  table is finished !"
+
+spark-submit --master yarn \
+  --conf spark.executor.memory=4g \
+  --conf spark.dynamicAllocation.maxExecutors=20 \
+  --conf spark.app.name=alarm_system  \
+  --conf spark.executor.memoryOverhead=2048 \
+  --jars s3://vomkt-emr-rec/jar/vova-bd-monitor/javamail.jar \
+  --class com.vova.bigdata.sparkbatch.monitor.MonitorMain s3://vomkt-emr-rec/jar/vova-bd-monitor/vova-bigdata-monitor-main.jar \
+  --envFile prod --db dwb --tlb dwb_fd_realtime_new_rpt --op check_index,send_message \
+  --date ${pt} --hour ${hour}
