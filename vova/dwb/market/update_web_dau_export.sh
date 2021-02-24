f45b1dd5d70f0b2924ace38d68eb274af6c508db
zyzheng <zyzheng@i9i8.com>
2021-02-24 11:47:53 +0800

    zyzheng

---
+++ vova/dwb/market/update_web_dau_export.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#指定日期和引擎
+cur_date=$1
+#默认日期为昨天
+if [ ! -n "$1" ];then
+cur_date=`date -d "-1 day" +%Y-%m-%d`
+fi
+
+sqoop export \
+-Dorg.apache.sqoop.export.text.dump_data_on_error=true \
+-Dsqoop.export.records.per.statement=500 \
+-Dmapreduce.job.queuename=important \
+--connect jdbc:mysql://db-logistics-w.gitvv.com:3306/themis_logistics_report?rewriteBatchedStatements=true \
+--username vvreport4vv --password 'nTTPdJhVp!DGv5VX4z33Fw@tHLmIG8oS' \
+--table dwb_vova_market_web_dau \
+--update-key event_date,datasource,region_code \
+--update-mode allowinsert \
+--m 1 \
+--hcatalog-database dwb \
+--hcatalog-table dwb_vova_market_web_dau \
+--hcatalog-partition-keys pt \
+--hcatalog-partition-values ${cur_date} \
+--fields-terminated-by '\001'
+
+#如果脚本失败，则报错
+if [ $? -ne 0 ];then
+  exit 1
+fi
