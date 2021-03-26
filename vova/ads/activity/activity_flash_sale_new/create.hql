f94d75dead98f0d43e4894792b7de1964dba7c21
ted.wan <ted.wan@vova.com.hk>
2021-03-26 10:09:10 +0800

    add new task

--- vova/ads/activity/activity_flash_sale_new/create.hql
+++ vova/ads/activity/activity_flash_sale_new/create.hql
@@ -11,3 +11,15 @@ create external table if  not exists  ads.ads_vova_activity_flash_sale_new (
     `rank`                      bigint COMMENT 'd_排名'
 ) COMMENT 'men cloth and shoes 活动表' PARTITIONED BY (pt string)
      STORED AS PARQUETFILE;
+
+
+CREATE TABLE `tmp.tmp_flash_sale_all`(
+  `goods_id` bigint COMMENT 'd_商品id',
+  `region_id` int COMMENT 'd_国家id',
+  `expre_cnt` int COMMENT 'i_曝光量',
+  `clk_cnt` int COMMENT 'i_点击量',
+  `ord_cnt` int COMMENT 'i_订单量',
+  `first_cat_id` int COMMENT 'i_一级品类id',
+  `second_cat_id` int COMMENT 'i_二级品类id',
+  `gmv` decimal(13,2) COMMENT 'i_gmv',
+  `click_uv` int COMMENT 'i_点击uv')STORED AS PARQUETFILE;
--- vova/ads/activity/activity_flash_sale_new/update.sh
+++ vova/ads/activity/activity_flash_sale_new/update.sh
@@ -6,7 +6,8 @@ if [ ! -n "$1" ]; then
   pt=$(date -d "-1 hour" +'%Y-%m-%d-%H')
 fi
 sql="
-WITH tmp_all AS (
+
+insert overwrite table tmp.tmp_flash_sale_all
 SELECT
     cb.goods_id,
     cb.region_id,
@@ -21,9 +22,10 @@ FROM
     dwd.dwd_vova_activity_goods_ctry_behave cb
 WHERE
     cb.pt = date(date_sub(substr('${pt}',0,10),2))
-    AND cb.region_id in (0, 4003, 4056, 4017, 4143, 3858 )
-    ),
-tmp_red_package_goods(
+    AND cb.region_id in (0, 4003, 4056, 4017, 4143, 3858 );
+
+
+with tmp_red_package_goods(
 SELECT
     goods_id,
     1 as type,
@@ -66,7 +68,7 @@ from
   tmp_red_package_goods.type,
   row_number() over(partition by tmp_red_package_goods.type order by tmp_red_package_goods.gsn_status desc, ord_cnt/expre_cnt desc) rank
   from
-  tmp_all
+  tmp.tmp_flash_sale_all tmp_all
   INNER join tmp_red_package_goods on tmp_all.goods_id = tmp_red_package_goods.goods_id
   where region_id = 0
 )
@@ -91,7 +93,7 @@ from
   tmp_red_package_goods.gsn_status,
   row_number() over(partition by tmp_red_package_goods.type order by tmp_red_package_goods.gsn_status desc, rand() desc) rank
   from
-  tmp_all
+  tmp.tmp_flash_sale_all tmp_all
   INNER join tmp_red_package_goods on tmp_all.goods_id = tmp_red_package_goods.goods_id
   where expre_cnt < 1000
   and region_id = 0
@@ -99,34 +101,8 @@ from
 )
 where rank<=100
 ),
-tmp_top_hot_sell(
-select
-goods_id,
-first_cat_id,
-second_cat_id,
-type,
-region_id,
-rank
-from
-(
-  select
-  tmp_all.goods_id,
-  first_cat_id,
-  second_cat_id,
-  tmp_all.region_id,
-  tmp_red_package_goods.type,
-  tmp_red_package_goods.gsn_status,
-  row_number() over(partition by tmp_red_package_goods.type,region_id order by tmp_red_package_goods.gsn_status desc, if(type=1, gmv / click_uv * clk_cnt / expre_cnt, ord_cnt/expre_cnt ) desc) rank
-  from
-  tmp_all
-  INNER join tmp_red_package_goods on tmp_all.goods_id = tmp_red_package_goods.goods_id
-)
-where rank<=30
-)
 
-
-
-INSERT overwrite TABLE ads.ads_vova_activity_flash_sale_new partition (pt='${pt}')
+tmp_for_you(
 select
 goods_id,
 region_id,
@@ -160,6 +136,118 @@ SELECT
 FROM
     tmp_rand_top_list
 )
+),
+tmp_for_you_sup(
+select
+goods_id,
+0 as region_id,
+if(type=1,'on-sale-picked-for-you','up-coming-picked-for-you') as biz_type,
+3 as rp_type,
+first_cat_id,
+nvl(second_cat_id,0) second_cat_id,
+rank +10000 as rank
+from
+(
+  select
+  /*+ repartition(50) */
+  tmp_all.goods_id,
+  tmp_all.first_cat_id,
+  tmp_all.second_cat_id,
+  clk_cnt / expre_cnt ctr,
+  tmp_red_package_goods.type,
+  row_number() over(partition by tmp_red_package_goods.type,tmp_all.first_cat_id order by tmp_red_package_goods.gsn_status desc, clk_cnt / expre_cnt desc) rank
+  from
+  tmp.tmp_flash_sale_all tmp_all
+  INNER join tmp_red_package_goods on tmp_all.goods_id = tmp_red_package_goods.goods_id
+  where region_id = 0
+  and exists (select 1 from (select first_cat_id,biz_type from tmp_for_you group by first_cat_id,biz_type)tmp_for_you where tmp_all.first_cat_id = tmp_for_you.first_cat_id and ((tmp_red_package_goods.type = 1 and tmp_for_you.biz_type='on-sale-picked-for-you') or (tmp_red_package_goods.type = 2 and tmp_for_you.biz_type='up-coming-picked-for-you')))
+  and not exists (select 1 from (select distinct goods_id from tmp_for_you)tmp_for_you where tmp_all.goods_id = tmp_for_you.goods_id)
+)
+where rank<=50
+),
+
+
+tmp_top_hot_sell(
+select
+goods_id,
+first_cat_id,
+second_cat_id,
+type,
+region_id,
+rank
+from
+(
+  select
+  tmp_all.goods_id,
+  first_cat_id,
+  second_cat_id,
+  tmp_all.region_id,
+  tmp_red_package_goods.type,
+  tmp_red_package_goods.gsn_status,
+  row_number() over(partition by tmp_red_package_goods.type,region_id order by tmp_red_package_goods.gsn_status desc, if(type=1, gmv / click_uv * clk_cnt / expre_cnt, ord_cnt/expre_cnt ) desc) rank
+  from
+  tmp.tmp_flash_sale_all tmp_all
+  INNER join tmp_red_package_goods on tmp_all.goods_id = tmp_red_package_goods.goods_id
+)
+where rank<=30
+),
+
+tmp_top_hot_sell_sup(
+select
+goods_id,
+region_id,
+if(type=1,'on-sale-hot-selling','up-coming-hot-selling') as biz_type,
+3 as rp_type,
+first_cat_id,
+nvl(second_cat_id,0) second_cat_id,
+rank+10000 as rank
+from
+(
+  select
+  /*+ repartition(50) */
+  tmp_all.goods_id,
+  first_cat_id,
+  second_cat_id,
+  tmp_all.region_id,
+  tmp_red_package_goods.type,
+  tmp_red_package_goods.gsn_status,
+  row_number() over(partition by tmp_red_package_goods.type,region_id,first_cat_id order by tmp_red_package_goods.gsn_status desc, if(type=1, gmv / click_uv * clk_cnt / expre_cnt, ord_cnt/expre_cnt ) desc) rank
+  from
+  tmp.tmp_flash_sale_all tmp_all
+  INNER join tmp_red_package_goods on tmp_all.goods_id = tmp_red_package_goods.goods_id
+  where
+  exists (select 1 from (select first_cat_id,type from tmp_top_hot_sell group by first_cat_id,type)tmp_top_hot_sell where tmp_all.first_cat_id = tmp_top_hot_sell.first_cat_id and tmp_red_package_goods.type = tmp_top_hot_sell.type)
+  and not exists (select 1 from (select distinct goods_id  from tmp_top_hot_sell) tmp_top_hot_sell where tmp_all.goods_id = tmp_top_hot_sell.goods_id)
+)
+where rank<=30
+)
+
+
+
+INSERT overwrite TABLE ads.ads_vova_activity_flash_sale_new partition (pt='${pt}')
+select
+goods_id,
+region_id,
+biz_type,
+rp_type,
+first_cat_id,
+nvl(second_cat_id,0) second_cat_id,
+rank
+FROM
+tmp_for_you
+
+union  all
+
+select
+goods_id,
+region_id,
+biz_type,
+rp_type,
+first_cat_id,
+nvl(second_cat_id,0) second_cat_id,
+rank
+FROM
+tmp_for_you_sup
 
 union all
 
@@ -173,6 +261,20 @@ select
     rank
 from
 tmp_top_hot_sell
+
+
+union  all
+
+select
+goods_id,
+region_id,
+biz_type,
+rp_type,
+first_cat_id,
+nvl(second_cat_id,0) second_cat_id,
+rank
+FROM
+tmp_top_hot_sell_sup
 ;
 
 "
@@ -181,6 +283,7 @@ echo $sql
 
 #如果使用spark-sql运行，则执行spark-sql -e
 spark-sql \
+--driver-memory 6G \
 --executor-memory 4G --executor-cores 1 \
 --conf "spark.sql.parquet.writeLegacyFormat=true"  \
 --conf "spark.dynamicAllocation.minExecutors=10" \
