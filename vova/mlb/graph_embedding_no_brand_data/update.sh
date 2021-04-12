7803a0b15fac83ddb992442ae5251dd81de2dbe2
hanjuntao <jthan@vova.com.hk>
2021-04-12 15:00:38 +0800

    update

--- vova/mlb/graph_embedding_no_brand_data/update.sh
+++ vova/mlb/graph_embedding_no_brand_data/update.sh
@@ -8,7 +8,7 @@ fi
 
 echo ${pre_date}
 
-file_num=`aws s3 ls s3://vova-mlb/REC/data/match/match_result/graph_embedding/no_brand/pt=${pre_date}/ | wc -l`
+file_num=`aws s3 ls s3://vova-mlb/REC/data/match/match_result/graph_embedding/brand_serial/pt=${pre_date}/ | wc -l`
 if [ ${file_num} -eq 0 ]; then
   echo "pt=${pre_date} file num = 0"
   exit 1
@@ -22,43 +22,44 @@ fi
 
 
 mysql -h rec-bi.cluster-cznqgcwo1pjt.us-east-1.rds.amazonaws.com -u bdwriter -pDd7LvXRPDP4iIJ7FfT8e <<EOF
-drop table if exists themis.graph_embedding_no_brand_data_new;
-drop table if exists themis.graph_embedding_no_brand_data_pre;
-CREATE TABLE themis.graph_embedding_no_brand_data_new
+drop table if exists themis.ads_rec_m_graph_embedding_nb_d_new;
+drop table if exists themis.ads_rec_m_graph_embedding_nb_d_pre;
+CREATE TABLE themis.ads_rec_m_graph_embedding_nb_d_new
 (
-    id1      bigint COMMENT 'id1',
-    id2      bigint COMMENT 'id2',
-    score    DOUBLE COMMENT 'score',
+    goods_id      bigint COMMENT 'goods_id',
+    rec_goods_id_list      varchar(4096) COMMENT '推荐商品列表',
+    score_list    varchar(4096) COMMENT '推荐商品分数列表',
     cur_date TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0)  COMMENT '日期',
-    PRIMARY KEY (id1, id2)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='keywords_clustering_result';
+    PRIMARY KEY (goods_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='graph_embedding召回结果表';
 EOF
 
 sqoop export \
 -Dorg.apache.sqoop.export.text.dump_data_on_error=true \
 --connect jdbc:mysql://rec-bi.cluster-cznqgcwo1pjt.us-east-1.rds.amazonaws.com:3306/themis \
 --username bdwriter --password Dd7LvXRPDP4iIJ7FfT8e \
---table graph_embedding_no_brand_data_new \
+--table ads_rec_m_graph_embedding_nb_d_new \
 --m 1 \
 --hcatalog-database mlb \
 --hcatalog-table mlb_vova_graph_embedding_no_brand_data \
 --hcatalog-partition-keys pt \
 --hcatalog-partition-values ${pre_date} \
---columns id1,id2,score \
---fields-terminated-by '\001'
+--columns goods_id,rec_goods_id_list,score_list \
+--fields-terminated-by '\t'
 
 if [ $? -ne 0 ];then
   exit 1
 fi
 
 mysql -h rec-bi.cluster-cznqgcwo1pjt.us-east-1.rds.amazonaws.com -u bdwriter -pDd7LvXRPDP4iIJ7FfT8e <<EOF
-rename table themis.graph_embedding_no_brand_data to themis.graph_embedding_no_brand_data_pre;
-rename table themis.graph_embedding_no_brand_data_new to themis.graph_embedding_no_brand_data;
+rename table themis.ads_rec_m_graph_embedding_nb_d to themis.ads_rec_m_graph_embedding_nb_d_pre;
+rename table themis.ads_rec_m_graph_embedding_nb_d_new to themis.ads_rec_m_graph_embedding_nb_d;
 EOF
 
 #如果脚本失败，则报错
 if [ $? -ne 0 ];then
   exit 1
 fi
+
 
 
