9836005c7f767826ffdf546d776e212325df28ff
hanjuntao <jthan@vova.com.hk>
2021-04-23 15:46:38 +0800

    update

--- vova/dwb/payment/update.sh
+++ vova/dwb/payment/update.sh
@@ -292,3 +292,17 @@ spark-sql \
 if [ $? -ne 0 ];then
   exit 1
 fi
+
+spark-submit  \
+--conf spark.executor.memory=4g \
+--conf spark.dynamicAllocation.maxExecutors=60 \
+--conf spark.app.name=vova_mlb_pay_send_knock \
+--conf spark.executor.memoryOverhead=2048 \
+--class com.vova.bigdata.sparkbatch.dataprocess.dwb.PaymentRate  s3://vomkt-emr-rec/jar/vova_mlb_pay_send_knock.jar \
+${cur_date}
+
+
+#如果脚本失败，则报错
+if [ $? -ne 0 ];then
+  exit 1
+fi
