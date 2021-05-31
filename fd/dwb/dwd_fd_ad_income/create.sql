1204e6bc7c8248c2677c651ad366117bf713a7a2
hanjuntao <jthan@vova.com.hk>
2021-05-31 16:28:34 +0800

    update

---
+++ fd/dwb/dwd_fd_ad_income/create.sql
@@ -0,0 +1,81 @@
+CREATE external TABLE IF NOT EXISTS dwb.dwb_ad_income_order
+(
+    cat_name       STRING,
+    day_value         STRING,
+    day_rate           STRING,
+    month_value         STRING,
+    month_rate      STRING,
+    rn      int
+)  PARTITIONED BY(pt string)
+row format delimited fields terminated by '\001'  STORED AS PARQUETFILE
+;
+
+CREATE external TABLE IF NOT EXISTS dwb.dwb_ad_income_out_stock
+(
+    cat_name       STRING,
+    day_value         STRING,
+    day_rate           STRING,
+    month_value         STRING,
+    month_rate      STRING,
+    rn      int
+)  PARTITIONED BY(pt string)
+row format delimited fields terminated by '\001'  STORED AS PARQUETFILE
+;
+
+CREATE external TABLE IF NOT EXISTS tmp.tmp_ad_income_01
+(
+    day_order_cnt         int,
+    day_gmv           decimal(15,4),
+    day_bonus         decimal(15,4),
+    day_shou_express      decimal(15,4),
+
+    mon_order_cnt         int,
+    mon_gmv           decimal(15,4),
+    mon_bonus         decimal(15,4),
+    mon_shou_express      decimal(15,4),
+
+    day_before_month_rate decimal(15,4),
+    month_before_month_rate decimal(15,4),
+    union_cost_day decimal(15,4),
+    unit_cost_month decimal(15,4),
+    order_amount_day decimal(15,4),
+    order_amount_month decimal(15,4)
+)
+row format delimited fields terminated by '\001'  STORED AS PARQUETFILE
+;
+
+CREATE external TABLE IF NOT EXISTS tmp.tmp_ad_income_02
+(
+    ad_cost_day         decimal(15,4),
+    ad_cost_month           decimal(15,4)
+)
+row format delimited fields terminated by '\001'  STORED AS PARQUETFILE
+;
+
+CREATE external TABLE IF NOT EXISTS tmp.tmp_ad_income_03
+(
+    day_order_cnt         int,
+    day_gmv           decimal(15,4),
+    day_bonus         decimal(15,4),
+    day_shou_express      decimal(15,4),
+
+    mon_order_cnt         int,
+    mon_gmv           decimal(15,4),
+    mon_bonus         decimal(15,4),
+    mon_shou_express      decimal(15,4),
+
+    day_before_month_rate decimal(15,4),
+    month_before_month_rate decimal(15,4),
+    union_cost_day decimal(15,4),
+    unit_cost_month decimal(15,4),
+    order_amount_day decimal(15,4),
+    order_amount_month decimal(15,4),
+    should_express_amount_day decimal(15,4),
+    should_express_amount_month decimal(15,4),
+    order_dim_day int,
+    order_dim_month int,
+    ad_cost_day decimal(15,4),
+    ad_cost_month decimal(15,4)
+)
+row format delimited fields terminated by '\001'  STORED AS PARQUETFILE
+;
